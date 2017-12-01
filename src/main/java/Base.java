bc4cdc8d24d0200f084ac2e714ce88ea3777a39c
gilushka <EzhiKN@gmail.com>
2017-12-01 18:31:56 +0300

    Добавлено чтение с клавиатуры

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Base {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Выберете операцию (символы +, -, * или /):");
+        String oper = scanner.next();
 
         System.out.println("Введите первое число:");
+        double num1 = scanner.nextInt();
 
         System.out.println("Введите второе число:");
+        double num2 = scanner.nextInt();
 
         System.out.printf("");
     }
