48b969efbe1f6fc526333322f77e77f84943e376
gilushka <EzhiKN@gmail.com>
2017-12-01 19:07:33 +0300

    Добавлен вывод

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -13,16 +13,32 @@ public class Base {
          */
 
         Scanner scanner = new Scanner(System.in);
+         double res=0;
 
-        System.out.println("Выберете операцию (символы +, -, * или /):");
-        String oper = scanner.next();
+        System.out.println("Выберете операцию (сложение - 1, вычитание - 2, умножение - 3, деление - 4):");
+        int oper = scanner.nextInt();
 
         System.out.println("Введите первое число:");
-        double num1 = scanner.nextInt();
+        double num1 = scanner.nextDouble();
 
         System.out.println("Введите второе число:");
-        double num2 = scanner.nextInt();
+        double num2 = scanner.nextDouble();
 
-        System.out.printf("");
+         switch(oper) {
+             case 1:
+                 res = num1 + num2;
+                 break;
+             case 2:
+                 res = num1 - num2;
+                 break;
+             case 3:
+                 res = num1 * num2;
+                 break;
+             case 4:
+                 res = num1 / num2;
+                 break;
+         }
+
+        System.out.printf("%+010.4f", res);
     }
 }
