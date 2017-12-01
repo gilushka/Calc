4c4d193424ec4b8b22208708c264faa8f0f8790b
gilushka <EzhiKN@gmail.com>
2017-12-01 18:25:43 +0300

    Операторы вывода

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,12 @@
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Тырьям-пам-пам!");
+
+        System.out.println("Выберете операцию (символы +, -, * или /):");
+
+        System.out.println("Введите первое число:");
+
+        System.out.println("Введите второе число:");
+
+        System.out.printf("");
     }
 }
