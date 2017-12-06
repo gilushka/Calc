7c81b76d71fb0c779814751bbf09624fda91b413
gilushka <EzhiKN@gmail.com>
2017-12-06 18:47:57 +0300

    Вынес калькулятор в отдельный класс

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,3 +1,5 @@
+import Calc.Calculator;
+
 import java.util.Scanner;
 
 /**
@@ -7,47 +9,21 @@ import java.util.Scanner;
 public class Base {
 
     /**
-     * Выходных параметров нет, метод осуществляет вывод в
-     * консоль результатов расчетов. При этом позволяет выбрать тип
-     * проводимой операции.
-     * В каком месте должны ставиться доки? На что тут ругается IDEA??
+     * Основной модуль программы. С него вызывается всё остальное.
      */
 
     public static void main(String[] args) {
-
+        System.out.println("Выберете что вы хотите делать дальше.");
+        System.out.println("Если вы хотите поработать с калькулятором, нажмите 1, если с массивами - 2");
         Scanner scanner = new Scanner(System.in);
-        double res = 0;
-
-        System.out.println("Выберете операцию (+, -, *, /):");
-
-        String sOper = scanner.next();
-        char cOper = sOper.charAt(0);
-
-        System.out.println("Введите первое число:");
-        double num1 = scanner.nextDouble();
-
-        System.out.println("Введите второе число:");
-        double num2 = scanner.nextDouble();
-
-        switch (cOper) {
-            case '+':
-                res = num1 + num2;
-                break;
-            case '-':
-                res = num1 - num2;
-                break;
-            case '*':
-                res = num1 * num2;
-                break;
-            case '/':
-                res = num1 / num2;
-                break;
-            default:
-                System.out.println("Некорректная операция");
-                break;
+        int iOper = scanner.nextInt();
+        if (iOper == 1){
+            Calculator calculator = new Calculator();
+            calculator.start();
         }
-
-        System.out.printf("%+5.4f", res);
+        else
+            System.out.println("Массивы находятся в процессе производства");
         scanner.close();
+
     }
 }
