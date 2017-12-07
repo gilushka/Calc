0af7c30c2cf0f23c66cdff61df0d6a7fc0d24076
gilushka <EzhiKN@gmail.com>
2017-12-07 17:20:16 +0300

    Добавлен вывод результатов

--- src/main/java/Backcounting/Backcounting.java
+++ src/main/java/Backcounting/Backcounting.java
@@ -9,19 +9,35 @@ public class Backcounting {
         for (int i = 0; i < 20; i++) {
             iArray[i] = (int) (Math.random()*(20+1)) - 10;
         }
-        int iMax = 0, iMin = 0, iMaxInd = -1, iMinInd = -1;
         for (int i = 0; i < 20; i++) {
-            if (iArray[i] > iMax){
+            System.out.println("Исходный массив:");
+            System.out.print(iArray[i] + " ");
+        }
+        System.out.println();
+        int iMax = -10, iMin = 10, iMaxInd = -1, iMinInd = -1;
+        for (int i = 0; i < 20; i++) {
+            if ((iArray[i] > iMax) && (iArray[i] < 0)){
                 iMax = iArray[i];
                 iMaxInd = i;
             }
-            if (iArray[i] < iMin){
+            if ((iArray[i] < iMin) && (iArray[i] > 0)){
                 iMin = iArray[i];
                 iMinInd = i;
             }
+        }
+        if ((iMaxInd >=0) && (iMinInd >= 0)){
+            iArray[iMinInd] = iMax;
+            iArray[iMaxInd] = iMin;
+            for (int i = 0; i < 20; i++) {
+                System.out.println("Итоговый массив: ");
+                System.out.print(iArray[i] + " ");
+            }
+        }
+        else
+            System.out.println("Не соблюдены условия задачи");
 //            System.out.println((i+1) + "-й элемент :" + iArray[i]);
 
-        }
+
 
     }
 
